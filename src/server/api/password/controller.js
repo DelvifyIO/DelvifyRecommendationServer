7b0814150de8bd0deb1f7ded548956c76f5d8e55
hooiyung <yung@delvify.io>
2020-02-03 14:08:21 +0800

    add avantlink password

--- src/server/api/password/controller.js
+++ src/server/api/password/controller.js
@@ -1,5 +1,10 @@
 const checkPassword = (req, res) => {
-    if (req.query.password === 'Delvify10Demo') {
+    const password = req.query.password;
+    const site = req.query.site || 'default';
+    if (site === 'avantLink' && password === 'AvantLinkDemo') {
+        return res.send('OK');
+    }
+    if (site === 'default' && password === 'Delvify10Demo') {
         return res.send('OK');
     } else {
         res.status(401).send('Incorrect Password');
